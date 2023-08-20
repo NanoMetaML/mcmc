023c533c5acea44bd5be6c55d3671da28a7633ba
michaelBezick <mbezick@purdue.edu>
2023-08-20 12:55:38 -0400

    Added transition_matrix calculation

---
+++ test/t1_MCMC/transition_matrix.py
@@ -0,0 +1,25 @@
+import mcmc
+import torch
+import numpy as np
+from itertools import product
+import math as m
+
+def calc_transition_matrix(n, H, temperature, proposal_distribution='uniform'):
+    if proposal_distribution == 'uniform':
+        previous_states = torch.tensor(list(product([0, 1], repeat=n))).float()
+        transition_states = torch.tensor(list(product([0, 1], repeat=n))).float()
+        proposal_probability = 1 / (2 ** n)
+        transition_matrix = torch.zeros((n, n))
+
+        for i in range(n):
+            for j in range(n):
+                previous_state_energy = mcmc.energyFns.quboEnergy(previous_states[i], H)
+                transition_state_energy = mcmc.energyFns.quboEnergy(transition_states[j], H)
+
+                acceptance_probability = min(1, m.e ** ((previous_state_energy - transition_state_energy) / temperature))
+
+                transition_matrix[i, j] = proposal_probability * acceptance_probability
+
+        return transition_matrix
+
+transition_matrix = calc_transition_matrix(6, torch.rand((6, 6)), 0.1)
