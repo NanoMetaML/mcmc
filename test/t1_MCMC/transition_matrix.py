cd579c7974e4f576451a7ecaa81470a633dc4bdc
michaelBezick <mbezick@purdue.edu>
2023-08-21 11:11:43 -0400

    Added batched support

--- test/t1_MCMC/transition_matrix.py
+++ test/t1_MCMC/transition_matrix.py
@@ -9,21 +9,28 @@ def calc_transition_matrix(n, H, temperature, proposal_distribution='uniform'):
     Creates a transition matrix where the rows represent the current state of the Markov chain and
     the columns represent the possible states to transition to. All entires are the probabilities
     of transitioning to a given state.
+
+    If H is batched (batch_size, n, n), returns transition matrix of shape (batch_size, 2 ** n, 2 ** n)
+    If H is singular, either (1, n, n) or (n, n), returns transition matrix of shape (1, 2 ** n, 2 ** n)
     """
 
     if proposal_distribution == 'uniform':
-        previous_states = torch.tensor(list(product([0, 1], repeat=n))).float()
-        transition_states = torch.tensor(list(product([0, 1], repeat=n))).float()
+
+        if (len(H.size()) == 2):
+            H = H.unsqueeze(0)
+        
+        previous_states = torch.tensor(list(product([0, 1], repeat=n))).float().unsqueeze(0).repeat(H.size()[0], 1, 1)
+        transition_states = previous_states
+        transition_matrix = torch.zeros((H.size()[0], 2 ** n, 2 ** n))
         proposal_probability = 1 / (2 ** n)
-        transition_matrix = torch.zeros((n, n))
 
-        for i in range(n):
-            for j in range(n):
-                previous_state_energy = mcmc.energyFns.quboEnergy(previous_states[i], H)
-                transition_state_energy = mcmc.energyFns.quboEnergy(transition_states[j], H)
+        for i in range(2 ** n):
+            for j in range(2 ** n):
+                previous_state_energy = mcmc.energyFns.quboEnergy(previous_states[:, i, :], H)
+                transition_state_energy = mcmc.energyFns.quboEnergy(transition_states[:, j, :], H)
 
-                acceptance_probability = min(1, m.e ** ((previous_state_energy - transition_state_energy) / temperature))
+                acceptance_probability = torch.min(torch.ones(H.size()[0]), m.e ** ((previous_state_energy - transition_state_energy) / temperature)).detach()
 
-                transition_matrix[i, j] = proposal_probability * acceptance_probability
+                transition_matrix[:, i, j] = torch.mul(proposal_probability, acceptance_probability)
 
-        return transition_matrix
+    return transition_matrix
